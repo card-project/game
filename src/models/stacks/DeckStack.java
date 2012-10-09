ac9480925e060cc9838b7046fe4859f44792c84d
Simon RENOULT <g4llic4@gmail.com>
2012-10-09 17:06:09 +0200

    Add singleton pattern to deckStack and discardStack

--- src/models/stacks/DeckStack.java
+++ src/models/stacks/DeckStack.java
@@ -2,5 +2,13 @@ package models.stacks;
 
 
 public class DeckStack extends GameStack {
+	private DeckStack() { }
 	
+	private static class DeckStackHolder {
+		private final static DeckStack INSTANCE = new DeckStack();
+	}
+	
+	public static DeckStack getInstance() {
+		return DeckStackHolder.INSTANCE;
+	}
 }
--- src/models/stacks/DiscardStack.java
+++ src/models/stacks/DiscardStack.java
@@ -1,7 +1,13 @@
 package models.stacks;
 
-
-
 public class DiscardStack extends GameStack {
-
+	private DiscardStack() { }
+	
+	private static class DiscardStackHolder {
+		private static final DiscardStack INSTANCE = new DiscardStack();
+	}
+	
+	public static DiscardStack getInstance() {
+		return DiscardStackHolder.INSTANCE;
+	}
 }
