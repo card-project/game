f0941f84169a27100520784c039b8c5ff9f7313e
Simon RENOULT <g4llic4@gmail.com>
2012-10-11 15:52:05 +0200

    Split into separated method AI and human instructions

--- src/views/terminal/Main.java
+++ src/views/terminal/Main.java
@@ -4,6 +4,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import models.Game;
+import models.cards.Card;
 import models.exceptions.AILevelOutOfBoundsException;
 import models.exceptions.AliasAlreadyChosenException;
 import models.exceptions.DiscardChoiceOutOfBoundsException;
@@ -18,11 +19,11 @@ public class Main {
 	public static void main( String[] args ) {
 
 		Game g = new Game();
-		
+
 		Main.setGameOptions( g );
 		Main.play( g );
 	}
-	
+
 	private static void setGameOptions( Game g ) {
 		Boolean userChoiceIsIncorrect = null;
 
@@ -33,14 +34,14 @@ public class Main {
 			userChoiceIsIncorrect = false;
 			try {
 				g.setNbPlayers( new Scanner( System.in ).nextInt() );
-			} catch (InputMismatchException e) {
+			} catch ( InputMismatchException e ) {
 				userChoiceIsIncorrect = true;
 				System.out.println( "Please enter a number." );
-			} catch (PlayersNumberException e) {
+			} catch ( PlayersNumberException e ) {
 				System.out.println( e.getMessage() );
 				userChoiceIsIncorrect = true;
 			}
-		} while (userChoiceIsIncorrect);
+		} while ( userChoiceIsIncorrect );
 
 		/* HUMAN PLAYERS NUMBER */
 
@@ -49,17 +50,17 @@ public class Main {
 			userChoiceIsIncorrect = false;
 			try {
 				g.setHumanPlayers( new Scanner( System.in ).nextInt() );
-			} catch (InputMismatchException e) {
+			} catch ( InputMismatchException e ) {
 				userChoiceIsIncorrect = true;
 				System.out.println( "Please enter a number." );
-			} catch (PlayersNumberException e) {
+			} catch ( PlayersNumberException e ) {
 				System.out.println( e.getMessage() );
 				userChoiceIsIncorrect = true;
-			} catch (TooManyHumanPlayersException e) {
+			} catch ( TooManyHumanPlayersException e ) {
 				System.out.println( e.getMessage() );
 				userChoiceIsIncorrect = true;
 			}
-		} while (userChoiceIsIncorrect);
+		} while ( userChoiceIsIncorrect );
 
 		/* DISTANCE GOAL */
 
@@ -68,30 +69,30 @@ public class Main {
 			userChoiceIsIncorrect = false;
 			try {
 				g.setGoal( new Scanner( System.in ).nextInt() );
-			} catch (InputMismatchException e) {
+			} catch ( InputMismatchException e ) {
 				userChoiceIsIncorrect = true;
 				System.out.println( "Please enter a number." );
-			} catch (DistanceGoalException e) {
+			} catch ( DistanceGoalException e ) {
 				userChoiceIsIncorrect = true;
 				System.out.println( e.getMessage() );
 			}
-		} while (userChoiceIsIncorrect);
+		} while ( userChoiceIsIncorrect );
 
 		/* HUMAN PLAYERS ALIAS */
 
-		for (int i = 0, j = i + 1; i < g.getPlayers().length; i++, j++) {
-			if (g.getPlayers()[i] instanceof HumanPlayer) {
+		for ( int i = 0, j = i + 1; i < g.getPlayers().length; i++, j++ ) {
+			if ( g.getPlayers()[i] instanceof HumanPlayer ) {
 				do {
 					System.out.print( "What is Player " + j + " alias ? " );
 					userChoiceIsIncorrect = false;
 					try {
-						g.setPlayerName( g.getPlayers()[i],
-								new Scanner ( System.in ).nextLine() );
-					} catch (AliasAlreadyChosenException e) {
+						g.setPlayerName( g.getPlayers()[i], new Scanner(
+								System.in ).nextLine() );
+					} catch ( AliasAlreadyChosenException e ) {
 						userChoiceIsIncorrect = true;
 						System.out.println( e.getMessage() );
 					}
-				} while (userChoiceIsIncorrect);
+				} while ( userChoiceIsIncorrect );
 			}
 		}
 
@@ -101,30 +102,31 @@ public class Main {
 
 		/* AI PLAYERS LEVEL */
 
-		for (Player p : g.getPlayers()) {
-			if (p instanceof AIPlayer) {
+		for ( Player p : g.getPlayers() ) {
+			if ( p instanceof AIPlayer ) {
 				do {
 					System.out.print( "What is " + p.getAlias()
 							+ " (AI) level ? " );
 					userChoiceIsIncorrect = false;
 					try {
-						g.setAIPlayerLevel( (AIPlayer) p, new Scanner( System.in ).nextInt() );
-					} catch (InputMismatchException e) {
+						g.setAIPlayerLevel( (AIPlayer) p, new Scanner(
+								System.in ).nextInt() );
+					} catch ( InputMismatchException e ) {
 						userChoiceIsIncorrect = true;
 						System.out.println( "Please enter a number." );
-					} catch (AILevelOutOfBoundsException e) {
+					} catch ( AILevelOutOfBoundsException e ) {
 						userChoiceIsIncorrect = true;
 						System.out.println( e.getMessage() );
 					}
-				} while (userChoiceIsIncorrect);
+				} while ( userChoiceIsIncorrect );
 			}
 		}
 
 		/* GAME PREPARATION */
 
 		g.prepare();
-		System.out.println( "Shuffling cards..." );
-		
+		System.out.println( '\n' + "Shuffling cards..." );
+
 		/* PLAYERS HAND */
 
 		g.preparePlayersHand();
@@ -132,74 +134,30 @@ public class Main {
 	}
 
 	private static void play( Game g ) {
-		
+
 		/* DECIDING FIRST PLAYER */
-	
+
 		Integer firstPlayerIndex = g.getFirstPlayerIndex();
-		System.out.println( "Deciding who will begin..." );
-		System.out.println( g.getPlayers()[firstPlayerIndex].getAlias()
+		System.out.println( "Deciding who will begin..." 
+				+ g.getPlayers()[firstPlayerIndex].getAlias()
 				+ " begins." );
-		
+
 		/* GAME PLAYING */
-		
+
 		Boolean gameIsOver = false;
 		Integer currentPlayerIndex = firstPlayerIndex;
 		do {
 			Player currentPlayer = g.getPlayers()[currentPlayerIndex];
-			
-			System.out.println( currentPlayer.toString() 
-					+ ( !g.getDiscardStack().isEmpty() ?  '\n' + "DISCARD STACK : " + g.getDiscardStack() : "" ) );
-			
-			// STEP 1 : Draw a card (discard or deck stack)
-			
-			System.out.println( "-> DRAWING STEP" );
-			if ( g.getDiscardStack().isEmpty() ) {
-				System.out.println( "Deck stack automatically chosen (discard stack is empty)." );
-				currentPlayer.draw( g.getDeckStack() );
+
+			if ( currentPlayer instanceof HumanPlayer ) {
+				humanPlay( g, (HumanPlayer) currentPlayer );
 			} else {
-				Boolean userChoiceIsIncorrect = false;
-				do {
-					System.out.println( "[D]eck or [d]iscard ?" );
-					// FIXME
-					String userChoice = new Scanner( System.in ).nextLine();
-					if ( userChoice.isEmpty() || userChoice.equals( "D" ) ) {
-						currentPlayer.draw( g.getDeckStack() );
-					} else if ( userChoice.equals( "d") ) {
-						currentPlayer.draw( g.getDiscardStack() );
-					} else {
-						userChoiceIsIncorrect = true;
-					}
-				} while ( userChoiceIsIncorrect );
+				AIPlay( g, (AIPlayer) currentPlayer );
 			}
-			
-			// STEP 2 : Play a card 
-			System.out.println( "-> PLAYING STEP" );
-			
-			// TODO Handle playing routine
-			
-			// STEP 3 : Discard a card
-			if ( currentPlayer.getHandStack().size() > Game.MAX_HAND_CARDS ) {
-				System.out.println( "-> DISCARDING STEP" );
-				Boolean userChoiceIsIncorrect = null;
-				do {
-					System.out.println( "Too many cards in hand, you must discard one." + '\n' 
-							+ "Which card do you discard ? Choose its index (from 1 to 5)." + '\n' 
-							+ currentPlayer.getHandStack() + '\n' );
-					userChoiceIsIncorrect = false;
-					try {
-						currentPlayer.discard( new Scanner( System.in ).nextInt(), g.getDiscardStack() );
-					} catch ( InputMismatchException e ) {
-						userChoiceIsIncorrect = true;
-						System.out.println( "Please enter a number." );
-					} catch ( DiscardChoiceOutOfBoundsException e ) {
-						userChoiceIsIncorrect = true;
-						System.out.println( e.getMessage() );
-					}
-				} while (userChoiceIsIncorrect);
-			}
-			
+
 			// STEP 4 : Check if game is over
-			if ( currentPlayer.getKilometers() == g.getGoal() || g.getDeckStack().isEmpty() ) {
+			if ( currentPlayer.getKilometers() == g.getGoal()
+					|| g.getDeckStack().isEmpty() ) {
 				gameIsOver = true;
 			} else {
 				// STEP 5 : Switch to the next player
@@ -208,5 +166,66 @@ public class Main {
 				}
 			}
 		} while ( !gameIsOver );
+	}
+	
+	private static void humanPlay( Game g, HumanPlayer player ) {
+		System.out.println( '\n' + player.toString() + ( !g.getDiscardStack().isEmpty() ? '\n'
+					+ "DISCARD STACK : " + g.getDiscardStack() : "" ) );
+
+		// STEP 1 : Draw a card (discard or deck stack)
+		
+		System.out.println( "-> DRAWING STEP" );
+
+		if ( g.getDiscardStack().isEmpty() ) {
+			System.out.println( "Discard stack is empty, deck stack is chosen." );
+			Card c = player.draw( g.getDeckStack() );
+			System.out.println( "DRAWN : " + c );
+		} else {
+			Boolean userChoiceIsIncorrect = false;
+			do {
+				System.out.println( "[D]eck or [d]iscard ?" );
+				@SuppressWarnings("resource")
+				String userChoice = new Scanner( System.in ).nextLine();
+				if ( userChoice.isEmpty() || userChoice.equals( "D" ) ) {
+					player.draw( g.getDeckStack() );
+				} else if ( userChoice.equals( "d" ) ) {
+					player.draw( g.getDiscardStack() );
+				} else {
+					userChoiceIsIncorrect = true;
+				}
+			} while ( userChoiceIsIncorrect );
+		}
+
+		// STEP 2 : Play a card
+		System.out.println( "-> PLAYING STEP" );
+		
+
+		// STEP 3 : Discard a card
+		if ( player.getHandStack().size() > Game.MAX_HAND_CARDS ) {
+			System.out.println( "-> DISCARDING STEP" );
+			Boolean userChoiceIsIncorrect = null;
+			do {
+				System.out.println( "Too many cards in hand, you must discard one."
+					+ "\n Which card do you discard ? Choose its index (from 1 to 5)."
+					+ '\n' + player.getHandStack() + '\n' );
+				
+				userChoiceIsIncorrect = false;
+				try {
+					player.discard( new Scanner( System.in ).nextInt(), g.getDiscardStack() );
+				} catch ( InputMismatchException e ) {
+					userChoiceIsIncorrect = true;
+					System.out.println( "Please enter a number." );
+				} catch ( DiscardChoiceOutOfBoundsException e ) {
+					userChoiceIsIncorrect = true;
+					System.out.println( e.getMessage() );
+				}
+			} while ( userChoiceIsIncorrect );
+		}
+	}
+	
+	private static void AIPlay ( Game g, AIPlayer p ) {
+		// STEP 1 : Draw a card
+		// STEP 2 : Play a card
+		// STEP 3 : Discard a card
 	}
 }
