9218a945cfa88f83c9183acb4d135f3292a5da7c
Simon RENOULT <g4llic4@gmail.com>
2012-11-01 16:54:13 +0100

    Add game view processing. Set as stable 1.0 version.

--- src/controllers/tui/TUIController.java
+++ src/controllers/tui/TUIController.java
@@ -1,45 +1,45 @@
 package controllers.tui;
 
-import views.tui.TUIMenu;
 import models.Game;
+import views.tui.TUIGameView;
+import views.tui.TUIMenuView;
 
 /**
  * Main TUI (Textual User Interface) controller.
  * 
  * Represents the interaction between model and TUI view.<br />
- * It owns the unique {@link Game} model class and handle both
- * view and model.
+ * It owns the unique {@link Game} model class and handle both view and model.
  * 
  * @author Simon RENOULT
- * @version 0.1
+ * @version 1.0
  */
 public class TUIController {
-	
+
 	// ------------ ATTRIBUTES ------------ //
-	
+
 	private Game currentGame;
-	
+
 	// ------------ CONSTRUCTORS ------------ //
-	
+
 	/**
-	 * Build the unique TUIController object and assign 
-	 * the predefined Game object to the object attribute.
+	 * Build the unique TUIController object and assign the predefined Game
+	 * object to the object attribute.
 	 * 
-	 * @param g Game object to work on.
+	 * @param g
+	 *            Game object to work on.
 	 */
 	public TUIController( Game g ) {
 		this.currentGame = g;
 	}
-	
+
 	// ------------ METHODS ------------ //
-	
+
 	/**
-	 * Unique public object method, it initiates the
-	 * MVC management processes.
+	 * Unique public object method, it initiates the MVC management processes.
 	 */
 	public void run() {
-		new TUIMenuController( new TUIMenu(), currentGame ).run();
-		// new TUIGameController ( new TUIGame(), currentGame ); 
+		new TUIMenuController( new TUIMenuView(), currentGame ).run();
+		new TUIGameController( new TUIGameView(), currentGame ).run();
 	}
-	
+
 }
