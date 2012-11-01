cf1d89a8136e3d5c14c00ac8a5ebdf6a68d6dfaf
Simon RENOULT <g4llic4@gmail.com>
2012-11-01 16:17:41 +0100

    Add empty draw/play/discard method. Set as non-developed yet. Version 0.1.

--- src/models/players/AIPlayer.java
+++ src/models/players/AIPlayer.java
@@ -1,18 +1,45 @@
 package models.players;
 
+import models.cards.Card;
+import models.moves.Move;
 import models.players.strategies.Strategy;
 
+/**
+ * Virtual player.
+ * 
+ * @author Simon RENOULT
+ * @version 0.1
+ */
 public class AIPlayer extends Player {
+
+	// ------------ ATTRIBUTES ------------ //
+	
 	private Integer level;
 	private Strategy strategy;
 
-	public void setLevel( Integer level ) {
-		this.level = level;
+	// ------------ METHODS ------------ //
+	
+	public void discard() {
+		
 	}
-
+	
+	public Card draw() {
+		return null;
+		
+	}
+	
+	public void player( Move m ) {
+		
+	}
+	
 	@Override
 	public String toString() {
 		return this.alias + " (" + this.level + ").";
 	}
 
+	// ------------ SETTERS ------------ //
+	
+	public void setLevel( Integer level ) {
+		this.level = level;
+	}
 }
