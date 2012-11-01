21e05674c42f31cd9ea2ebf83d1ce961f083d083
Simon RENOULT <g4llic4@gmail.com>
2012-11-01 16:53:12 +0100

    Set as stable 1.0 version.

--- src/models/stacks/HandStack.java
+++ src/models/stacks/HandStack.java
@@ -1,7 +1,12 @@
 package models.stacks;
 
 
+/**
+ * @author Simon RENOULT
+ * @version 1.0
+ */
 public class HandStack extends PlayerStack {
-	public static final int REGULAR_CARD_NB = 4; 
-	public static final int MAX_CARD_NB = 5; 
+	public static final int MAX_IN_PLAY_CARD = 5; 
+	public static final int MAX_CARD_NB = 4; 
+	public static final int MIN_CARD_NB = 1;
 }
