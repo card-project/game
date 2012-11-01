2f456bb3d5f6392d7b29f4d4a22947cc0e3ea755
Simon RENOULT <g4llic4@gmail.com>
2012-11-01 16:26:02 +0100

    Refactoring method name.

--- src/models/stacks/DistanceStack.java
+++ src/models/stacks/DistanceStack.java
@@ -1,18 +1,37 @@
 package models.stacks;
 
 import models.cards.Card;
-import models.cards.distances.Distance200;
+import models.cards.distances.DistanceCard;
 
+/**
+ * @author Simon RENOULT
+ * @version 1.0
+ */
 public class DistanceStack extends PlayerStack {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -8792591903898303572L;
 
+	/**
+	 * Get the amount of driven kilometers.
+	 * 
+	 * @return the amount of driven kilometers.
+	 */
+	public int getTravelledDistance() {
+		int currentDistance = 0;
+		for( Card c : this ) {
+			currentDistance += ( (DistanceCard ) c ).getRange();
+		}
+		
+		return currentDistance;
+	}
+	
 	public Boolean maxNumberOfDistance200IsReached() {
 		int distance200Counter = 0;
 		for ( Card c : this ) {
-			if ( c instanceof Distance200 ) {
+			if ( c instanceof DistanceCard ) {
+				if ( ( ( DistanceCard ) c ).getRange() == 200 )
 				distance200Counter++;
 			}
 		}
