b2c019e86c7640901ec488128ef0b3ba7bcaa9db
Simon RENOULT <g4llic4@gmail.com>
2012-11-13 16:05:47 +0100

    Set target player/card verification.

--- src/models/moves/BasicMove.java
+++ src/models/moves/BasicMove.java
@@ -9,8 +9,8 @@ import models.exceptions.IllegalMoveException;
 import models.players.Player;
 
 /**
- * @author G4llic4
- *
+ * @author Simon RENOULT
+ * @version 0.1
  */
 public class BasicMove extends Move {
 	
@@ -24,6 +24,24 @@ public class BasicMove extends Move {
 	
 	public BasicMove( Player origin ) {
 		this.source = origin;
+	}
+	
+	// ------------ METHODS ------------ //
+
+	private boolean cardAndTargetAreCompatible( Player target ) {
+		if ( target.equals( this.source ) ) {
+			if ( this.cardToPlay instanceof RemedyCard 
+					|| this.cardToPlay instanceof DistanceCard 
+					|| this.cardToPlay instanceof SafetyCard  ) {
+				return true;
+			}
+		} else if ( ! target.equals( this.source ) ){
+			if ( this.cardToPlay instanceof HazardCard ) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 	
 	// ------------ GETTERS ------------ //
@@ -51,22 +69,29 @@ public class BasicMove extends Move {
 	 * @throws IllegalMoveException
 	 */
 	public void setCardToPlay( Card c ) {
-		if ( c instanceof HazardCard ) {
+		if ( c == null ) {
+			throw new IllegalAccessError( "Card must be defined." );
+		} else {
 			this.cardToPlay = c;			
-		} else if ( c instanceof RemedyCard ) {
-			if ( source.getBattleStack().get() )
-		} else if ( c instanceof DistanceCard ) {
-			
-		} else if ( c instanceof SafetyCard ) {
-			
 		}
 	}
 	
-	public void setTarget( Player player ) {
-		this.target = player;
-	}
-	
-	public void setSource( Player source ) {
-		this.source = source;
+	/**
+	 * Define the target of the chosen card.
+	 * 
+	 * @param targetPlayer Target chosen for the selected card.
+	 * @throws IllegalMoveException Target cannot be chosen with this card.
+	 * @throws IllegalAccessError No card previously selected.
+	 */
+	public void setTarget( Player targetPlayer ) throws IllegalMoveException, IllegalAccessError {
+		if ( this.cardToPlay == null ) {
+			throw new IllegalAccessError( "No card selected." );
+		} else {
+			if ( this.cardAndTargetAreCompatible( targetPlayer ) ) {
+				this.target = targetPlayer;
+			} else {
+				throw new IllegalMoveException( "Target must be an opponent." );
+			}
+		}
 	}
 }
