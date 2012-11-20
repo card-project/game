210c2bfc112df746a13a81682e4fa1ede1519d8a
Simon RENOULT <g4llic4@gmail.com>
2012-11-20 14:31:55 +0100

    User the new Card architecture.

--- src/models/cards/CardFactory.java
+++ src/models/cards/CardFactory.java
@@ -1,29 +1,12 @@
 package models.cards;
 
-import models.cards.distances.DistanceCard;
-import models.cards.hazards.Accident;
-import models.cards.hazards.FlatTire;
-import models.cards.hazards.OutOfGas;
-import models.cards.hazards.SpeedLimit;
-import models.cards.hazards.Stop;
-import models.cards.remedies.EndOfLimit;
-import models.cards.remedies.Gasoline;
-import models.cards.remedies.GoRoll;
-import models.cards.remedies.Repairs;
-import models.cards.remedies.SpareTire;
-import models.cards.safeties.DrivingAce;
-import models.cards.safeties.ExtraTank;
-import models.cards.safeties.PunctureProof;
-import models.cards.safeties.RightOfWay;
-
-
 /**
  * Card class factory.
  * 
  * Allow lighter/softer dependencies between classes.
  * 
  * @author Simon RENOULT
- * @version 1.0
+ * @version 1.1
  */
 public class CardFactory {
 	public static Card createCard( CardType ct ) {
@@ -39,37 +22,39 @@ public class CardFactory {
 			return new DistanceCard( 100 );
 		case Distance200:
 			return new DistanceCard( 200 );
-		// Hazards
+			// Hazards
 		case Accident:
-			return new Accident();
+			return new HazardCard( CardFamily.StateOfCar, CardType.Accident );
 		case FlatTire:
-			return new FlatTire();
+			return new HazardCard( CardFamily.Tire, CardType.FlatTire );
 		case OutOfGas:
-			return new OutOfGas();
+			return new HazardCard( CardFamily.Gas, CardType.OutOfGas );
 		case SpeedLimit:
-			return new SpeedLimit();
+			return new HazardCard( CardFamily.Speed, CardType.SpeedLimit );
 		case Stop:
-			return new Stop();
-		// Remedies
+			return new HazardCard( CardFamily.GoStop, CardType.Stop );
+			// Remedies
 		case GoRoll:
-			return new GoRoll();
+			return new RemedyCard( CardFamily.GoStop, CardType.GoRoll );
 		case EndOfLimit:
-			return new EndOfLimit();
+			return new RemedyCard( CardFamily.Speed, CardType.EndOfLimit );
 		case Gasoline:
-			return new Gasoline();
+			return new RemedyCard( CardFamily.Gas, CardType.Gasoline );
 		case Repairs:
-			return new Repairs();
+			return new RemedyCard( CardFamily.StateOfCar, CardType.Repairs );
 		case SpareTire:
-			return new SpareTire();
-		// Safeties
+			return new RemedyCard( CardFamily.Tire, CardType.SpareTire );
+			// Safeties
 		case DrivingAce:
-			return DrivingAce.getInstance();
+			return new SafetyCard( CardFamily.StateOfCar, CardType.DrivingAce );
 		case ExtraTank:
-			return ExtraTank.getInstance();
+			return new SafetyCard( CardFamily.Gas, CardType.ExtraTank );
 		case PunctureProof:
-			return PunctureProof.getInstance();
+			return new SafetyCard( CardFamily.Tire, CardType.PunctureProof );
 		case RightOfWay:
-			return RightOfWay.getInstance();
+			SafetyCard sc = new SafetyCard( CardFamily.GoStop, CardType.RightOfWay );
+			sc.addFamily( CardFamily.Speed );
+			return sc;
 		default:
 			return null;
 		}
