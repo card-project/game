ca95be989eb33778b78592b40cc16c9bacccb109
Adrien SAUNIER <contact@alwin.fr>
2012-11-24 10:09:32 +0100

    Driver stategy : chooseCardToPlay()

--- src/models/players/strategies/Driver.java
+++ src/models/players/strategies/Driver.java
@@ -1,10 +1,28 @@
 package models.players.strategies;
 
+import java.util.LinkedList;
+
 import models.cards.Card;
+import models.cards.distances.DistanceCard;
+import models.cards.hazards.HazardCard;
+import models.cards.hazards.SpeedLimit;
+import models.players.AIPlayer;
 import models.stacks.GameStack;
 
 public class Driver implements Strategy {
 
+	// ------------ ATTRIBUTES ------------ //
+	
+	private AIPlayer player;
+	
+	// ------------ CONSTRUCTORS ------------ //
+	
+	public Driver(AIPlayer player) {
+		this.player = player;
+	}
+	
+	// ------------ METHODS ------------ //
+	
 	@Override
 	public GameStack chooseStackToDraw() {
 		// TODO Auto-generated method stub
@@ -13,13 +31,59 @@ public class Driver implements Strategy {
 
 	@Override
 	public Card chooseCardToPlay() {
-		// TODO Auto-generated method stub
-		return null;
+		DistanceCard chosenCard = null;
+		boolean speedLimit = false;
+		
+		if(this.player.getBattleStack().isAttacked())
+		{
+			LinkedList<Card> listInjuringCards = this.player.getBattleStack().getCards();
+			
+			for (Card card : listInjuringCards) {
+				
+				HazardCard injuringCard = (HazardCard) card;
+				
+				if(injuringCard instanceof SpeedLimit)
+				{
+					speedLimit = true;
+				}
+				else
+				{
+					
+				}	
+			}
+		}
+		
+		LinkedList<Card> list = this.player.getHandStack().getCards();
+		for (Card card : list) {
+			if(card instanceof DistanceCard)
+			{
+				DistanceCard cardChecked = (DistanceCard) card;
+				
+				if(!speedLimit ||(speedLimit && cardChecked.getRange() <= 50))
+				{					
+					if(cardChecked.getRange() == 200)
+					{
+						if(!this.player.getDistanceStack().maxNumberOfDistance200IsReached())
+						{
+							if(chosenCard.getRange() < cardChecked.getRange())
+								chosenCard = cardChecked;
+						}
+					}
+					else
+					{
+						if(chosenCard.getRange() < cardChecked.getRange())
+							chosenCard = cardChecked;
+					}
+				}
+			}
+		}
+		
+		return chosenCard;
 	}
 
 	@Override
 	public Card chooseCardToDiscard() {
-		// TODO Auto-generated method stub
+		//TODO
 		return null;
 	}
 
