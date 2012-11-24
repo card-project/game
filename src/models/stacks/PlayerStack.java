d15ce88342982b4aaac7dd2966bfe1a49a5a6854
Adrien SAUNIER <contact@alwin.fr>
2012-11-24 10:10:57 +0100

    New method : getCards()
    Return the list of cards for players

--- src/models/stacks/PlayerStack.java
+++ src/models/stacks/PlayerStack.java
@@ -1,5 +1,13 @@
 package models.stacks;
 
+import java.util.LinkedList;
+
+import models.cards.Card;
+
 public abstract class PlayerStack extends Stack {
+	
+	public LinkedList<Card> getCards() {
+		return this.cards;		
+	}
 
 }
