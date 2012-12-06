d4cc7c94eca55ac1f1cf312190e10c66795bc32d
Simon RENOULT <simon.renoult@gmail.com>
2012-12-06 15:32:28 +0100

    Add default constructor (without parameter) so explicit exception can be raised without any message.

--- src/models/exceptions/moveExceptions/IllegalMoveException.java
+++ src/models/exceptions/moveExceptions/IllegalMoveException.java
@@ -7,6 +7,9 @@ public class IllegalMoveException extends Exception {
 	 */
 	private static final long serialVersionUID = 2220602030025582655L;
 
+	public IllegalMoveException() {
+	}
+	
 	public IllegalMoveException( String string ) {
 		super( string );
 	}
