1f9bf33bfa72f8788f3c52ec422d4f63d1c3d3ac
Simon RENOULT <simon.renoult@gmail.com>
2012-12-09 13:53:07 +0100

    add tests

--- tests/models/cards/DistanceCardTest.java
+++ tests/models/cards/DistanceCardTest.java
@@ -1,25 +1,92 @@
 package models.cards;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import models.exceptions.IllegalCardTypeException;
+import models.players.HumanPlayer;
+import models.players.Player;
 
 import org.junit.Test;
 
 public class DistanceCardTest {
 
+	// ------------ CONSTANTS ------------ //
+	
+	private static final int GOAL = 700;
+	
 	// ------------ ATTRIBUTES ------------ //
 	
 	private DistanceCard c = (DistanceCard) CardFactory.createCard( CardType.Distance25 );
 	
 	// ------------ METHODS ------------ //
 	
-	@Test
-	public void testGetRange() {
+	@Test public void testGetRange() {
 		assertEquals( c.getRange(), 25 );
 	}
 	
-	public void testSetRange() {
+	@Test public void testSetRange() {
 		assertNotEquals( c.getRange(), 50 );
 		c.setRange( 50 );
 		assertEquals( c.getRange(), 50 );
+	}
+	
+	@Test public void testIsPlayableOn() throws IllegalCardTypeException {
+		Player p = new HumanPlayer();
+		
+		// -- > Case 1 : Not started yet
+		
+		assertFalse( c.isPlayableOn( p, GOAL ) );
+		
+		// -- > Setting context : Add initial GoRoll
+		
+		p.getBattleStack().push( CardFactory.createCard( CardType.GoRoll ) );
+		
+		// -- > Case 2 : Player has started
+		
+		assertTrue( c.isPlayableOn( p, GOAL ) );
+		
+		// -- > Setting context : Attacked player
+		
+		p.getBattleStack().push( CardFactory.createCard( CardType.Accident ) );
+		
+		// -- > Case 3 : Attacked player
+		
+		assertFalse( c.isPlayableOn( p, GOAL ) );
+
+		// -- > Setting context : Slowed player
+		
+		p.getBattleStack().pop();
+		p.getDistanceStack().push( CardFactory.createCard( CardType.SpeedLimit ) );
+		
+		// -- > Case 4 : Slowed player
+		
+		c.setRange( 200 );
+		assertFalse( c.isPlayableOn( p, GOAL ) );
+		
+		c.setRange( 50 );
+		assertTrue( c.isPlayableOn( p, GOAL ) );
+		
+		// -- > Reseting context
+		
+		p.getDistanceStack().pop();
+		
+		// -- > Setting context : Maximum 200
+
+		c.setRange( 200 );
+		p.getDistanceStack().push( CardFactory.createCard( CardType.Distance200 ) );
+			
+		// -- > Case 5 : Maximum 200 not reached
+	
+		assertTrue( c.isPlayableOn( p, GOAL ) );
+		
+		// -- > Setting context : Maximum 200
+
+		p.getDistanceStack().push( CardFactory.createCard( CardType.Distance200 ) );
+			
+		// -- > Case 5 : Maximum 200 not reached
+	
+		assertFalse( c.isPlayableOn( p, GOAL ) );
 	}
 }
---
+++ tests/models/cards/HazardCardTest.java
@@ -0,0 +1,69 @@
+package models.cards;
+
+import static org.junit.Assert.*;
+
+import models.exceptions.IllegalCardTypeException;
+import models.players.HumanPlayer;
+import models.players.Player;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HazardCardTest {
+
+	// ------------ ATTRIBUTES ------------ //
+	
+	private HazardCard c = (HazardCard) CardFactory.createCard( CardType.Accident );
+	
+	// ------------ METHODS ------------ //
+	
+	@Test public void testIsPlayableOn() throws IllegalCardTypeException {
+		Player p = new HumanPlayer();
+
+		// -- > Case 1 : Not started yet
+
+		assertFalse( c.isPlayableOn( p ) );
+		
+		
+		// -- > Case 2 : Player has started
+
+		p.getBattleStack().push( CardFactory.createCard( CardType.GoRoll ) );
+		assertTrue( c.isPlayableOn( p ) );
+		
+		// -- > Case 3 : Player is protected
+		
+		p.getSafetyStack().push( CardFactory.createCard( CardType.DrivingAce ) );
+		assertFalse( c.isPlayableOn( p ) );
+		p.getSafetyStack().pop();
+		
+		// -- > Case 4 : Player is not protected
+
+		assertTrue( c.isPlayableOn( p ) );
+		
+		// -- > Case 5 : Player is slowed
+		
+		p.getDistanceStack().push( CardFactory.createCard( CardType.SpeedLimit ) );
+		
+		c = (HazardCard) CardFactory.createCard( CardType.SpeedLimit );
+		
+		assertFalse( c.isPlayableOn( p ) );
+		p.getDistanceStack().pop();
+		
+		// -- > Case 6 : Player is not slowed
+
+		assertTrue( c.isPlayableOn( p ) );
+		
+		// -- > Case 7 : Player is attacked 
+		
+		p.getBattleStack().push( CardFactory.createCard( CardType.FlatTire ) );
+
+		c = (HazardCard) CardFactory.createCard( CardType.Accident );
+
+		assertFalse( c.isPlayableOn( p ) );
+		p.getBattleStack().pop();
+		
+		// -- > Case 6 : Player is not attacked
+
+		assertTrue( c.isPlayableOn( p ) );
+	}
+}
