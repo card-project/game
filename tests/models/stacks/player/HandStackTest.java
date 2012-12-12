7e36509db1e8f9c9aa6f1e7c41d0d830260f3e3b
Simon RENOULT <simon.renoult@gmail.com>
2012-12-12 10:56:24 +0100

    Remove abosolete methods.

--- tests/models/stacks/player/HandStackTest.java
+++ tests/models/stacks/player/HandStackTest.java
@@ -1,11 +1,9 @@
 package models.stacks.player;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import models.cards.Card;
 import models.cards.CardFactory;
 import models.cards.CardType;
-import models.cards.DistanceCard;
 import models.cards.HazardCard;
 import models.exceptions.IllegalCardTypeException;
 
@@ -41,86 +39,6 @@ public class HandStackTest {
 		HazardCard hc = (HazardCard) CardFactory.createCard( CardType.Accident );
 
 		assertTrue( handStack.hasRemedyFor( hc.getFamily() ) );
-	}
-
-	@Test
-	public void testContainsSafety() {
-		Card c = CardFactory.createCard( CardType.DrivingAce );
-
-		try {
-			handStack.push( c );
-		} catch ( IllegalCardTypeException e ) {
-			e.printStackTrace();
-		}
-
-		assertTrue( handStack.containsSafety() );
-	}
-
-	@Test
-	public void testContainsHazard() {
-		Card c = CardFactory.createCard( CardType.Accident );
-
-		try {
-			handStack.push( c );
-		} catch ( IllegalCardTypeException e ) {
-			e.printStackTrace();
-		}
-
-		assertTrue( handStack.containsHazard() );
-	}
-
-	@Test
-	public void testContainsRemedy() {
-		Card c = CardFactory.createCard( CardType.Repairs );
-
-		try {
-			handStack.push( c );
-		} catch ( IllegalCardTypeException e ) {
-			e.printStackTrace();
-		}
-
-		assertTrue( handStack.containsRemedy() );
-	}
-
-	@Test
-	public void testContainsDistance() {
-		testDistance( CardType.Distance25 );
-		testDistance( CardType.Distance50 );
-		testDistance( CardType.Distance75 );
-		testDistance( CardType.Distance100 );
-		testDistance( CardType.Distance200 );
-
-	}
-
-	private void testDistance( CardType distanceType ) {
-		try {
-			this.handStack.push( (DistanceCard) CardFactory
-					.createCard( distanceType ) );
-		} catch ( IllegalCardTypeException e ) {
-			e.printStackTrace();
-		}
-		assertTrue( handStack.containsDistance() );
-		this.handStack.getCards().clear();
-	}
-
-	@Test
-	public void testContainsSlowDistanceCard() {
-		testSlowDistance( CardType.Distance25, true );
-		testSlowDistance( CardType.Distance50, true );
-		testSlowDistance( CardType.Distance75, false );
-		testSlowDistance( CardType.Distance100, false );
-		testSlowDistance( CardType.Distance200, false );
-	}
-
-	private void testSlowDistance( CardType distanceType, boolean expected ) {
-		try {
-			this.handStack.push( (DistanceCard) CardFactory
-					.createCard( distanceType ) );
-		} catch ( IllegalCardTypeException e ) {
-			e.printStackTrace();
-		}
-		assertEquals( expected, handStack.containsSlowDistanceCard() );
-		this.handStack.getCards().clear();
 	}
 
 }
